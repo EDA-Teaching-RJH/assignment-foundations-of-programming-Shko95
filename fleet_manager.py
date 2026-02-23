72fad2c8ae5941a7b0dae0ffcfe3fc5941708889
Shko95 <sh102@kent.ac.uk>
2026-02-23 10:03:08 +0000

    start/changes

---
+++ fleet_manager.py
@@ -0,0 +1,9 @@
+
+def inti_database(): 
+
+    names = ["Carol", "Jack", "Spock", "Hikaru", "Kira"]
+    ranks = ["Captain", "Commander", "Lieutenant", "Major"]
+    divs = ["Stark", "Baratheon", "Snow", "Lannister", "Targaryen"]
+    ids =[]
+    return names, ranks, divs, ids
+
